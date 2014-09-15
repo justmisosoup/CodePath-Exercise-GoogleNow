179777b2e24585cc889327ad1e294c3783a2958d
Sara Menefee <s.menefee@me.com>
2014-09-15 15:11:47 -0700

    CodePath : GoogleNow

---
+++ GoogleNow/Welcome1ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Welcome1ViewController.swift
+//  GoogleNow
+//
+//  Created by Sara  on 9/15/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class Welcome1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func clickBackView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
